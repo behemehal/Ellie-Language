dbcff4c398919b0a6832873c34b0bf7cffb09c1d
Restyled.io <commits@restyled.io>
2021-06-02 13:54:34 +0000

    Restyled by clang-format

--- capi/main.cpp
+++ capi/main.cpp
@@ -2,13 +2,13 @@
 #include "stdio.h"
 
 int main() {
-    uint32_t x = 10;
+  uint32_t x = 10;
 
-    ParserOptions p;
+  ParserOptions p;
 
-    p.break_on_error = true;
+  p.break_on_error = true;
 
-    Parsed test = parser_new("d test : S = 10;", p);
+  Parsed test = parser_new("d test : S = 10;", p);
 
-    // printf("%d", test.syntax_errors[0]);
+  // printf("%d", test.syntax_errors[0]);
 }
