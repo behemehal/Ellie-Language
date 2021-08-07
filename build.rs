44e778ad28268bd193db65f4f4129584eb346a49
Ahmetcan Aksu <ahmetcanco@gmail.com>
2021-08-07 12:38:06 +0300

    Speed up build up to %96

--- build.rs
+++ build.rs
@@ -23,7 +23,7 @@ fn read_file(file_dir: &str) -> Result<String, String> {
 }
 
 fn resolve_import(lib_name: String) -> ellie_parser::parser::ResolvedImport {
-    std::println!(
+    std::eprintln!(
         "{}[ReadingFile]{}: {}~./lib/{}.ei{}",
         terminal_colors::get_color(terminal_colors::Colors::Magenta),
         terminal_colors::get_color(terminal_colors::Colors::Reset),
@@ -37,7 +37,7 @@ fn resolve_import(lib_name: String) -> ellie_parser::parser::ResolvedImport {
             file_content: parse(e, lib_name).parsed,
         },
         Err(err) => {
-            std::println!(
+            panic!(
                 "{}[Fail]{}: Cannot read file {}~./lib/{}.ei{}\n{:#?}",
                 terminal_colors::get_color(terminal_colors::Colors::Red),
                 terminal_colors::get_color(terminal_colors::Colors::Reset),
@@ -46,13 +46,12 @@ fn resolve_import(lib_name: String) -> ellie_parser::parser::ResolvedImport {
                 terminal_colors::get_color(terminal_colors::Colors::Reset),
                 err
             );
-            ellie_parser::parser::ResolvedImport::default()
         }
     }
 }
 
 fn parse(contents: String, file_name: String) -> ellie_parser::parser::ParserResponse {
-    std::println!(
+    std::eprintln!(
         "{}[ParsingFile]{}: {}~./lib/{}.ei{}",
         terminal_colors::get_color(terminal_colors::Colors::Cyan),
         terminal_colors::get_color(terminal_colors::Colors::Reset),
@@ -82,7 +81,7 @@ fn parse(contents: String, file_name: String) -> ellie_parser::parser::ParserRes
     let parsed = parser.map();
 
     if parsed.syntax_errors.len() == 0 {
-        std::println!(
+        std::eprintln!(
             "{}[ParsingSuccess]{}: {}~./lib/{}.ei{}",
             terminal_colors::get_color(terminal_colors::Colors::Green),
             terminal_colors::get_color(terminal_colors::Colors::Reset),
@@ -91,7 +90,7 @@ fn parse(contents: String, file_name: String) -> ellie_parser::parser::ParserRes
             terminal_colors::get_color(terminal_colors::Colors::Reset),
         );
     } else {
-        std::println!(
+        panic!(
             "{}[ParsingFailed]{}: {}~./lib/{}.ei{}",
             terminal_colors::get_color(terminal_colors::Colors::Red),
             terminal_colors::get_color(terminal_colors::Colors::Reset),
@@ -104,62 +103,105 @@ fn parse(contents: String, file_name: String) -> ellie_parser::parser::ParserRes
 }
 
 fn main() {
-    let ellie_lib = resolve_import("ellie".to_string());
-    let ellie_version: Vec<ellie_parser::syntax::file_key::FileKey> = ellie_lib
-        .file_content
-        .clone()
-        .items
-        .into_iter()
-        .filter_map(|x| {
-            if let ellie_parser::parser::Collecting::FileKey(e) = x {
-                if e.data.key_name == "version" {
-                    Some(e.data)
-                } else {
-                    None
+    match read_file(&("./lib/ellie.ei".to_string())) {
+        Ok(ellie_lib) => {
+            match read_file(&("./core/src/builded_libraries.rs".to_string())) {
+                Ok(current_lib) => {
+                    //@version *=\\s*\"\\^|\\~?(\\d|x|\\*)+\\.(\\d|x|\\*)+\\.(\\d|x|\\*)
+                    let version_line_regex = Regex::new(
+                        "(@(\\s)*version(\\s)*=)(\\s)*(\")*(?P<version>\"\\^|\\~?(\\d|x|\\*)+\\.(\\d|x|\\*)+\\.(\\d|x|\\*))*(\"|()*;)",
+                    )
+                    .unwrap();
+
+                    if version_line_regex.is_match(&ellie_lib.clone())
+                        && version_line_regex.is_match(&current_lib.clone())
+                    {
+                        let lib_version_number = &version_line_regex.captures(&ellie_lib).unwrap();
+                        let current_version_number =
+                            &version_line_regex.captures(&current_lib).unwrap();
+
+                        if lib_version_number.name("version").is_some()
+                            && current_version_number.name("version").is_some()
+                        {
+                            let lib_version = &lib_version_number["version"];
+                            let current_version = &current_version_number["version"];
+                            if lib_version == current_version {
+                                eprintln!(
+                                    "\nCompiling Ellie standard library {}v{}{} is not required",
+                                    terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                                    lib_version,
+                                    terminal_colors::get_color(terminal_colors::Colors::Reset),
+                                );
+                            } else {
+                                let ellie_lib =
+                                    parse(ellie_lib.clone(), "ellie".to_string()).parsed;
+
+                                eprintln!(
+                                    "\nCompiling Ellie standard library {}v{}{} complete",
+                                    terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                                    lib_version,
+                                    terminal_colors::get_color(terminal_colors::Colors::Reset),
+                                );
+                                let j = serde_json::to_string(&ellie_lib).unwrap();
+                                fs::write(
+                                    "./core/src/builded_libraries.rs",
+                                    format!("//@version = \"{}\";\npub static ELLIE_STANDARD_LIBRARY : &str = {:#?};", lib_version, j),
+                                )
+                                .unwrap();
+
+                                panic!("Compile {} {}", lib_version, current_version);
+                            }
+                        } else if lib_version_number.name("version").is_some() {
+                            panic!(
+                                "\nCompiling Ellie Failed, No version key found in {}~./core/src/builded_libraries.rs{}",
+                                terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                                terminal_colors::get_color(terminal_colors::Colors::Reset),
+                            );
+                        } else {
+                            panic!(
+                                "\nCompiling Ellie Failed, No version key found in {}~./lib/ellie.ei{}",
+                                terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                                terminal_colors::get_color(terminal_colors::Colors::Reset),
+                            );
+                        }
+                    } else {
+                        if version_line_regex.is_match(&ellie_lib.clone()) {
+                            panic!(
+                                "\nCompiling Ellie Failed, No version key found in {}~./core/src/builded_libraries.rs{}",
+                                terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                                terminal_colors::get_color(terminal_colors::Colors::Reset),
+                            );
+                        } else {
+                            panic!(
+                                "\nCompiling Ellie Failed, No version key found in {}~./lib/ellie.ei{}",
+                                terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                                terminal_colors::get_color(terminal_colors::Colors::Reset),
+                            );
+                        }
+                    }
                 }
-            } else {
-                None
+                Err(err) => {
+                    panic!(
+                        "{}[Fail]{}: Cannot read file {}~./core/{}.rs{}\n{:#?}",
+                        terminal_colors::get_color(terminal_colors::Colors::Red),
+                        terminal_colors::get_color(terminal_colors::Colors::Reset),
+                        terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                        "builded_libraries",
+                        terminal_colors::get_color(terminal_colors::Colors::Reset),
+                        err
+                    );
+                }
             }
-        })
-        .collect();
-
-    if ellie_version.len() == 0 {
-        println!(
-            "\nCompiling Ellie Failed, No version key found in {}ellie.ei{}",
-            terminal_colors::get_color(terminal_colors::Colors::Yellow),
-            terminal_colors::get_color(terminal_colors::Colors::Reset),
-        );
-    } else {
-        let version_key = &ellie_version[0].value;
-        if let ellie_parser::syntax::types::Types::String(e) = version_key {
-            let version_regex = Regex::new(r"(\^|\~?)(\d|x|\*)+\.(\d|x|\*)+\.(\d|x|\*)+").unwrap();
-
-            if version_regex.is_match(&e.value) {
-                println!(
-                    "\nCompiling Ellie standard library {}v{}{} complete",
-                    terminal_colors::get_color(terminal_colors::Colors::Yellow),
-                    e.value,
-                    terminal_colors::get_color(terminal_colors::Colors::Reset),
-                );
-                let j = serde_json::to_string(&ellie_lib.file_content).unwrap();
-                fs::write(
-                    "./core/src/builded_libraries.rs",
-                    format!("pub static ELLIE_STANDARD_LIBRARY : &str = {:#?};", j),
-                )
-                .unwrap();
-            } else {
-                println!(
-                    "\nCompiling Ellie Failed, Version key found in {}ellie.ei{} does not fit to versioning rules.",
-                    terminal_colors::get_color(terminal_colors::Colors::Yellow),
-                    terminal_colors::get_color(terminal_colors::Colors::Reset),
-                );
-            }
-        } else {
-            println!(
-                "\nCompiling Ellie Failed, Version key found in {}ellie.ei{} was '{}' but 'string' expected.",
-                terminal_colors::get_color(terminal_colors::Colors::Yellow),
+        }
+        Err(err) => {
+            panic!(
+                "{}[Fail]{}: Cannot read file {}~./lib/{}.ei{}\n{:#?}",
+                terminal_colors::get_color(terminal_colors::Colors::Red),
                 terminal_colors::get_color(terminal_colors::Colors::Reset),
-                version_key.get_type(),
+                terminal_colors::get_color(terminal_colors::Colors::Yellow),
+                "ellie",
+                terminal_colors::get_color(terminal_colors::Colors::Reset),
+                err
             );
         }
     }
