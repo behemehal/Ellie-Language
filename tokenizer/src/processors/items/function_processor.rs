ee6a8ac4426be7294dfb37fc2ca22c89c6cc8cd3
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-04-06 04:10:03 +0300

    Fix no return error

--- tokenizer/src/processors/items/function_processor.rs
+++ tokenizer/src/processors/items/function_processor.rs
@@ -156,6 +156,7 @@ impl crate::processors::Processor for function::FunctionCollector {
         } else if !self.return_collected {
             if self.data.return_type.complete && letter_char == '{' {
                 self.return_collected = true;
+                self.data.body_pos.range_start = cursor;
             } else if self.data.return_type.complete && letter_char == ';' {
                 self.data.hash = ellie_core::utils::generate_hash_u64();
                 self.data.defining = true;
@@ -165,7 +166,9 @@ impl crate::processors::Processor for function::FunctionCollector {
                 if self.data.return_pos.range_start.is_zero() && letter_char != ' ' {
                     self.data.return_pos.range_start = cursor;
                 }
-                self.data.return_pos.range_end = cursor;
+                if letter_char != ' ' {
+                    self.data.return_pos.range_end = cursor;
+                }
                 hang = self
                     .data
                     .return_type
@@ -173,11 +176,24 @@ impl crate::processors::Processor for function::FunctionCollector {
             }
         } else if letter_char == '}' && self.brace_count == 0 {
             self.complete = true;
+            self.data.body_pos.range_end = cursor;
             self.data.pos.range_end = cursor.clone().skip_char(1);
             self.data.hash = ellie_core::utils::generate_hash_u64();
             self.iterator.finalize();
             errors.extend(self.iterator.errors.clone());
             self.data.body = self.iterator.collected.clone();
+            let contains_ret = self.data.body.iter().any(|x| match x {
+                super::Processors::Ret(_) => true,
+                _ => false,
+            });
+
+            if !self.data.no_return && !contains_ret {
+                errors.push(error::error_list::ERROR_S2.clone().build(
+                    vec![],
+                    alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
+                    self.data.body_pos,
+                ));
+            }
         } else {
             if letter_char == '{' {
                 self.brace_count += 1;
