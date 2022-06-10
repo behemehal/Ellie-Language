583a6d10d1336f43870780fcd367b08420221a38
Ahmetcan Aksu <ahmetcanco@gmail.com>
2022-06-10 22:13:22 +0300

    Fix wrong syntax error behaviour

--- tokenizer/src/processors/types/float_processor.rs
+++ tokenizer/src/processors/types/float_processor.rs
@@ -6,7 +6,7 @@ impl crate::processors::Processor for float_type::FloatTypeCollector {
         &mut self,
         errors: &mut Vec<error::Error>,
         cursor: defs::CursorPosition,
-        _last_char: char,
+        last_char: char,
         letter_char: char,
     ) -> bool {
         if !self.at_point {
@@ -23,11 +23,21 @@ impl crate::processors::Processor for float_type::FloatTypeCollector {
                 ));
             }
         } else {
-            if letter_char.to_string().parse::<i8>().is_ok() {
+            if last_char == ' ' {
+                errors.push(error::error_list::ERROR_S1.clone().build(
+                    vec![error::ErrorBuildField {
+                        key: "token".to_string(),
+                        value: letter_char.to_string(),
+                    }],
+                    alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
+                    defs::Cursor::build_from_cursor(cursor),
+                ));
+            } else if letter_char.to_string().parse::<i8>().is_ok() {
                 self.point += &letter_char.to_string();
                 self.data.raw += &letter_char.to_string();
                 if let Ok(nm) = self.data.raw.parse::<f32>() {
                     self.data.value = nm;
+                    self.complete = true;
                 } else {
                     errors.push(error::error_list::ERROR_S16.clone().build(
                         vec![error::ErrorBuildField {
@@ -39,7 +49,7 @@ impl crate::processors::Processor for float_type::FloatTypeCollector {
                     ));
                 }
                 self.data.pos.range_end = cursor;
-            } else {
+            } else if letter_char != ' ' {
                 errors.push(error::error_list::ERROR_S1.clone().build(
                     vec![error::ErrorBuildField {
                         key: "token".to_string(),
