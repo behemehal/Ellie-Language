82d45b413d3bd23929ad6cb7ca621682e30f76d6
Ahmetcan Aksu <ahmetcanaksu@behemehal.org>
2023-11-14 14:31:03 +0300

    Implement constructor body in fmt

--- ellie_engine/fmt/src/renderer/items/constructor.rs
+++ ellie_engine/fmt/src/renderer/items/constructor.rs
@@ -28,7 +28,30 @@ impl CodeRenderer for Constructor {
         let mut constructor_input = format!("{state_scope_length}co{params}",);
 
         if !self.inside_code.is_empty() {
-            todo!()
+            if options.render_brace_next_line {
+                constructor_input += &format!(
+                    "{line_ending}{state_scope_length}{{{line_ending}",
+                    line_ending = options.render_line_ending()
+                );
+            } else {
+                constructor_input += &format!(
+                    " {{{line_ending}",
+                    line_ending = options.render_line_ending()
+                );
+            }
+
+            let mut child_state = state.clone();
+            child_state.scope_length += 1;
+            child_state.ending_token = options.render_line_ending();
+            for line in self.inside_code.iter() {
+                constructor_input += &line.render(&child_state, options);
+            }
+
+            constructor_input += format!(
+                "{state_scope_length}}}{line_ending}",
+                line_ending = options.render_line_ending()
+            )
+            .as_str();
         } else {
             constructor_input += &format!(";{}", options.render_line_ending());
         }
