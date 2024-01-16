b2af55906a6ad13ddc9cedcccd90bd186c475fa2
Ahmetcan Aksu <ahmetcanaksu@behemehal.org>
2024-01-16 21:10:56 +0300

    Update stack memory and stack size constants

--- ellie_engine/vm/src/config.rs
+++ ellie_engine/vm/src/config.rs
@@ -1,10 +1,10 @@
 // VM's stack memory size
 // 24 * 1024 = 24kb
-pub const STACK_MEMORY_SIZE: usize = 1024;
+pub const STACK_MEMORY_SIZE: usize = 2048;
 
 // VM's stack size
 // 168 * 512 = 86kb
-pub const STACK_SIZE: usize = 512;
+pub const STACK_SIZE: usize = 1024;
 
 //Program size
 // 48 * 4096 = 196kb
