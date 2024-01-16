1883891279a94743f902ce382403d0a3827067ff
Ahmetcan Aksu <ahmetcanaksu@behemehal.org>
2024-01-16 20:37:18 +0300

    Refactor import processor logic

--- ellie_engine/tokenizer/src/processors/items/import_processor.rs
+++ ellie_engine/tokenizer/src/processors/items/import_processor.rs
@@ -9,55 +9,67 @@ impl crate::processors::Processor for Import {
         last_char: char,
         letter_char: char,
     ) -> bool {
-        if !self.path_filled {
-            if letter_char == ':' && self.path != "" {
-                self.path_filled = true;
-            } else if letter_char == ';' && self.path != "" {
+        if !self.path_starter_collected {
+            if letter_char == '"' || letter_char == '@' {
+                self.path_starter_collected = true;
+                self.path_module = letter_char == '"';
+                self.link_module = letter_char == '@';
+                self.path_pos.range_start = cursor;
+            } else if letter_char != ' ' {
+                errors.push(error::error_list::ERROR_S1.clone().build(
+                    vec![error::ErrorBuildField {
+                        key: "token".to_string(),
+                        value: letter_char.to_string(),
+                    }],
+                    alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
+                    defs::Cursor::build_from_cursor(cursor),
+                ));
+            }
+        } else if self.path_module && !self.import_filled {
+            if letter_char == '"' {
+                self.import_filled = true;
+            } else {
+                self.path_pos.range_end = cursor;
+                self.path += &letter_char.to_string();
+            }
+        } else if self.link_module && !self.import_filled {
+            if utils::reliable_name_range(utils::ReliableNameRanges::VariableName, letter_char)
+                .reliable
+            {
+                self.path_pos.range_end = cursor;
+                self.path += &letter_char.to_string();
+            } else if (letter_char == ' ' || letter_char == ':' || letter_char == ';')
+                && self.reference != ""
+            {
+                self.complete = letter_char == ';';
+                self.reference_starter_collected = letter_char == ':';
+                self.import_filled = true;
+                self.pos.range_end = cursor;
+            } else {
+                errors.push(error::error_list::ERROR_S1.clone().build(
+                    vec![error::ErrorBuildField {
+                        key: "token".to_string(),
+                        value: letter_char.to_string(),
+                    }],
+                    alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
+                    defs::Cursor::build_from_cursor(cursor),
+                ));
+            }
+        } else if !self.reference_starter_collected {
+            if letter_char == ':' {
+                self.reference_starter_collected = true;
+            } else if letter_char == ';' {
                 self.pos.range_end = cursor;
                 self.complete = true;
-            } else if !self.path_module && !self.link_module {
-                if letter_char == '@' {
-                    self.link_module = true;
-                    self.path_pos.range_start = cursor;
-                } else if letter_char == '"' {
-                    self.path_module = true;
-                    self.path_pos.range_start = cursor;
-                }
-            } else if self.link_module {
-                if utils::reliable_name_range(utils::ReliableNameRanges::VariableName, letter_char)
-                    .reliable
-                {
-                    if last_char == ' ' {
-                        errors.push(error::error_list::ERROR_S1.clone().build(
-                            vec![error::ErrorBuildField {
-                                key: "token".to_string(),
-                                value: letter_char.to_string(),
-                            }],
-                            alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
-                            defs::Cursor::build_from_cursor(cursor),
-                        ));
-                    }
-                    self.path_pos.range_end = cursor;
-                    self.path += &letter_char.to_string();
-                } else if letter_char == ' ' && self.path != "" {
-                    self.path_filled = true;
-                } else if letter_char != ' ' {
-                    errors.push(error::error_list::ERROR_S1.clone().build(
-                        vec![error::ErrorBuildField {
-                            key: "token".to_string(),
-                            value: letter_char.to_string(),
-                        }],
-                        alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
-                        defs::Cursor::build_from_cursor(cursor),
-                    ));
-                }
-            } else {
-                if letter_char == '"' {
-                    self.path_filled = true;
-                } else {
-                    self.path_pos.range_end = cursor;
-                    self.path += &letter_char.to_string();
-                }
+            } else if letter_char != ' ' {
+                errors.push(error::error_list::ERROR_S1.clone().build(
+                    vec![error::ErrorBuildField {
+                        key: "token".to_string(),
+                        value: letter_char.to_string(),
+                    }],
+                    alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
+                    defs::Cursor::build_from_cursor(cursor),
+                ));
             }
         } else {
             if utils::reliable_name_range(utils::ReliableNameRanges::VariableName, letter_char)
@@ -80,6 +92,15 @@ impl crate::processors::Processor for Import {
             } else if letter_char == ';' && self.path != "" {
                 self.pos.range_end = cursor;
                 self.complete = true;
+            } else if letter_char != ' ' {
+                errors.push(error::error_list::ERROR_S1.clone().build(
+                    vec![error::ErrorBuildField {
+                        key: "token".to_string(),
+                        value: letter_char.to_string(),
+                    }],
+                    alloc::format!("{}:{}:{}", file!().to_owned(), line!(), column!()),
+                    defs::Cursor::build_from_cursor(cursor),
+                ));
             }
         }
         false
