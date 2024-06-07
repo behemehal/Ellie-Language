d60eec27986280b3f583d294bd07fea1bde597b2
ahmtcn123 <ahmetcanaksu@behemehal.org>
2024-06-07 17:44:13 +0300

    Implement conversion between string and class

--- ellie_engine/core/src/utils/mod.rs
+++ ellie_engine/core/src/utils/mod.rs
@@ -260,7 +260,7 @@ pub fn colapseable_operator(parent: Operators, child: Operators) -> bool {
                         },
                     }
                 }
-                Operators::AssignmentType(_) => todo!(),
+                Operators::AssignmentType(child_inner) => true,
                 Operators::Null => todo!(),
             }
         }
--- ellie_engine/vm/src/instructions/A2S.rs
+++ ellie_engine/vm/src/instructions/A2S.rs
@@ -56,16 +56,13 @@ impl super::InstructionExecuter for A2S {
                             StaticRawType::from_heap_reference(current_stack.get_pos());
                     }
                     11 => {
-
                         let class_ref = current_stack.registers.A.to_uint();
-                        let class = heap_memory.get(&class_ref);
-
-                        panic!("Conver: {:#?}, ", class);
-
-                        //let data = current_stack.registers.A.to_short().to_string();
-                        //heap_memory.set(&current_stack.get_pos(), RawType::generate_string(data));
-                        //current_stack.registers.A =
-                        //    StaticRawType::from_heap_reference(current_stack.get_pos());
+                        heap_memory.set(
+                            &current_stack.get_pos(),
+                            RawType::generate_string(format!("Class<{:#?}>", class_ref)),
+                        );
+                        current_stack.registers.A =
+                            StaticRawType::from_heap_reference(current_stack.get_pos());
                     }
                     e => {
                         return Err(ExecuterPanic {
