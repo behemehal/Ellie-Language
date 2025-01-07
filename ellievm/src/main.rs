56bf089d18e87b5e137636b51798761d97d0cd78
Ahmetcan Aksu <ahmetcanaksu@behemehal.org>
2025-01-08 00:56:56 +0300

    Add ffi for testing

--- ellievm/src/main.rs
+++ ellievm/src/main.rs
@@ -8,6 +8,7 @@ mod utils;
 #[macro_use]
 extern crate lazy_static;
 
+use core::{ffi::c_void, ptr::addr_of_mut};
 use ellie_engine::{
     ellie_core::defs::PlatformArchitecture,
     ellie_renderer_utils::{
@@ -24,6 +25,9 @@ use ellie_engine::{
     engine_constants,
     vm::{parse_debug_file, RFile},
 };
+
+use libffi::low::*;
+
 use run::VmSettings;
 use std::{
     ffi::CString, fs::File, io::Read, os::windows::io::AsRawHandle, path::Path, time::SystemTime,
@@ -317,6 +321,354 @@ fn main() {
                     let handle = Box::into_raw(handle);
 
                     VmNativeAnswer::Ok(((handle as *mut usize) as usize).into())
+                }),
+            )));
+
+            ellie_core_module.register_element(ModuleElements::Function(FunctionElement::new(
+                "load_library_handle",
+                Box::new(|_, args| {
+                    if args.is_empty() {
+                        return VmNativeAnswer::RuntimeError(
+                            "Signature mismatch expected 1 argument(s)".to_string(),
+                        );
+                    }
+
+                    let library_path = args
+                        .first()
+                        .unwrap()
+                        .data
+                        .clone()
+                        .into_string()
+                        .unwrap()
+                        .clone();
+
+                    unsafe {
+                        let library = libloading::Library::new(library_path.as_str()).unwrap();
+                        let handle = Box::new(library);
+                        let handle = Box::into_raw(handle);
+
+                        println!("Handle: {:?}", handle);
+                        //handle as *mut usize,
+
+                        VmNativeAnswer::Ok(((handle as *mut usize) as usize).into())
+                    }
+                }),
+            )));
+
+            ellie_core_module.register_element(ModuleElements::Function(FunctionElement::new(
+                "call_library_function",
+                Box::new(|_, args| {
+                    if args.len() != 3 {
+                        return VmNativeAnswer::RuntimeError(
+                            "Signature mismatch expected 3 argument(s)".to_string(),
+                        );
+                    }
+
+                    let library_handle = args[0].data.clone().into_integer().unwrap().as_usize;
+                    let function_name = args[1].data.clone().into_string().unwrap();
+
+                    println!("Library Handle: {:#?}", library_handle);
+                    println!("Function Name: {:#?}", function_name);
+
+                    unsafe {
+                        //let library = &*(library_handle as *const libloading::Library);
+
+                        let library = libloading::Library::new("user32.dll")
+                            .expect("Failed to load library, did you build c lib?");
+
+                        let function: libloading::Symbol<unsafe extern "C" fn()> =
+                            match library.get(function_name.as_str().as_bytes()) {
+                                Ok(e) => e,
+                                Err(e) => {
+                                    return VmNativeAnswer::RuntimeError(format!(
+                                        "Failed to get function '{}', ({})",
+                                        function_name,
+                                        e.to_string()
+                                    ));
+                                }
+                            };
+
+                        let mut ffi_type_args: Vec<*mut ffi_type> = Vec::new();
+                        let mut ffi_args: Vec<*mut c_void> = Vec::new();
+
+                        for cloak in args[2].data.clone().into_array().unwrap() {
+                            let cloak = cloak.into_array().unwrap();
+                            let type_id = cloak[0].clone().into_string().unwrap();
+                            let value = cloak[1].clone();
+
+                            println!("Setting up ffi type: {:#?}", type_id);
+
+                            //ffi_type_void
+                            //ffi_type_uint8
+                            //ffi_type_sint8
+                            //ffi_type_uint16
+                            //ffi_type_sint16
+                            //ffi_type_uint32
+                            //ffi_type_sint32
+                            //ffi_type_uint64
+                            //ffi_type_sint64
+                            //ffi_type_float
+                            //ffi_type_double
+                            //ffi_type_pointer
+
+                            match type_id.as_str() {
+                                "void" => ffi_type_args.push(core::ptr::addr_of_mut!(types::void)),
+                                "uint8" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::uint8))
+                                }
+                                "sint8" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::sint8))
+                                }
+                                "uint16" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::uint16))
+                                }
+                                "sint16" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::sint16))
+                                }
+                                "uint32" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::uint32))
+                                }
+                                "sint32" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::sint32))
+                                }
+                                "uint64" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::uint64))
+                                }
+                                "sint64" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::sint64))
+                                }
+                                "float" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::float))
+                                }
+                                "double" => {
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::double))
+                                }
+                                "pointer" => {
+                                    println!("SETTING UP POINTER");
+                                    ffi_type_args.push(core::ptr::addr_of_mut!(types::pointer))
+                                }
+                                _ => {
+                                    return VmNativeAnswer::RuntimeError(format!(
+                                        "Unknown ffi type '{}'",
+                                        type_id
+                                    ));
+                                }
+                            }
+
+                            match type_id.as_str() {
+                                "void" => {
+                                    if value.is_void() {
+                                        ffi_args.push(&0_u64 as *const _ as *mut c_void);
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'void' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "uint8" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as u8)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'uint8' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "sint8" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as i8)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'sint8' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "uint16" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as u16)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'uint16' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "sint16" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as i16)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'sint16' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "uint32" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as u32)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'uint32' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "sint32" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as i32)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'sint32' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "uint64" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as u64)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'uint64' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "sint64" => {
+                                    if value.is_integer() {
+                                        ffi_args.push(
+                                            &(value.into_integer().unwrap().as_usize as i64)
+                                                as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'sint64' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "float" => {
+                                    if value.is_float() {
+                                        ffi_args.push(
+                                            &(value.into_float().unwrap() as f32) as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'float' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "double" => {
+                                    if value.is_float() {
+                                        ffi_args.push(
+                                            &(value.into_float().unwrap() as f64) as *const _
+                                                as *mut c_void,
+                                        );
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'double' on ffi argument",
+                                        ));
+                                    }
+                                }
+                                "pointer" => {
+                                    if value.is_string() {
+                                        println!("Pointer: {:#?}", value);
+                                        let cstring =
+                                            CString::new(value.clone().into_string().unwrap().as_str())
+                                                .unwrap()
+                                                .into_bytes();
+                                        let cstring = &cstring.as_ptr() as *const _ as *mut c_void;
+                                        ffi_args.push(cstring);
+                                    } else {
+                                        return VmNativeAnswer::RuntimeError(format!(
+                                            "Expected 'pointer' string on ffi argument",
+                                        ));
+                                    }
+                                }
+                                _ => {
+                                    return VmNativeAnswer::RuntimeError(format!(
+                                        "Unknown ffi type '{}'",
+                                        type_id
+                                    ));
+                                }
+                            }
+                        }
+
+                        let mut cif: ffi_cif = Default::default();
+
+                        match prep_cif(
+                            &mut cif,
+                            libffi::low::ffi_abi_FFI_DEFAULT_ABI,
+                            args.len(),
+                            addr_of_mut!(types::sint32),
+                            ffi_type_args.as_mut_ptr(),
+                        ) {
+                            Ok(c) => c,
+                            Err(e) => {
+                                let reason = match e {
+                                    libffi::low::Error::Typedef => {
+                                        "Bad or unsupported type representation"
+                                    }
+                                    libffi::low::Error::Abi => "Given a bad or unsupported ABI",
+                                };
+                                return VmNativeAnswer::RuntimeError(format!(
+                                    "Failed to prepare ffi_cif ({})",
+                                    reason
+                                ));
+                            }
+                        }
+
+                        let fn_ptr = CodePtr::from_ptr(function.try_as_raw_ptr().unwrap());
+
+                        let text = CString::new("This is the title").unwrap().into_bytes();
+                        let text = &text.as_ptr() as *const _ as *mut c_void;
+                        let caption = CString::new("This is the caption").unwrap().into_bytes();
+                        let caption = &caption.as_ptr() as *const _ as *mut c_void;
+
+
+                        let mut values: [*mut c_void; 4] = [
+                            &0_u64 as *const _ as *mut c_void,                // HWND
+                            ffi_args[1],
+                            ffi_args[2],
+                            &0_u32 as *const _ as *mut c_void, // uType
+                        ];
+
+                        let mut ffi_args: &mut [*mut c_void] = &mut ffi_args;
+
+                        let result =
+                            libffi::low::call::<i32>(&mut cif, fn_ptr, values.as_mut_ptr());
+
+                        println!("MessageBoxW returned: {}", result);
+
+                        println!("ffi_type_args: {:#?}", ffi_type_args);
+                        println!("ffi_args: {:#?}", ffi_args);
+
+                        //let result = function();
+                        VmNativeAnswer::Ok(result.into())
+                    }
                 }),
             )));
 
